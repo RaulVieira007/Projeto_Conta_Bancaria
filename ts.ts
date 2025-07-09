01e5add0dade668457e223cd5be97c4994398632
RaulVieira007 <raul.vieiras2001@gmail.com>
2025-07-09 16:37:11 -0300

    update

--- ts.ts
+++ ts.ts
@@ -2,9 +2,11 @@ import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from "./src/util/model/conta";
 
-export function main(): void {
+export function main() {
+
     let opcao: number;
 
+    // Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
     conta.sacar(10500);
@@ -13,19 +15,17 @@ export function main(): void {
     conta.visualizar();
 
     while (true) {
-        console.clear(); // limpa o terminal
 
-        // Menu com cores
-        console.log(colors.bg.black, colors.fg.yellow);
-        console.log("*****************************************************");
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
         console.log("                                                     ");
-        console.log("                     BANCO RAIZ                      ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
         console.log("*****************************************************");
         console.log("                                                     ");
         console.log("            1 - Criar Conta                          ");
         console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Número              ");
+        console.log("            3 - Buscar Conta por Numero              ");
         console.log("            4 - Atualizar Dados da Conta             ");
         console.log("            5 - Apagar Conta                         ");
         console.log("            6 - Sacar                                ");
@@ -34,87 +34,83 @@ export function main(): void {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ");
-        console.log(colors.reset);
+        console.log("                                                     ", 
+        colors.reset);
 
-        // Input da opção
-        opcao = readlinesync.questionInt("Entre com a opção desejada: ");
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
 
-        // Encerrar programa
-        if (opcao === 9) {
-            console.log(colors.fg.greenstrong, "\nBanco-Raiz sempre com você.");
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
             console.log(colors.reset, "");
             process.exit(0);
         }
 
-        // Opções do menu
         switch (opcao) {
             case 1:
                 console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+                
+                keyPress()
                 break;
-
             case 2:
                 console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 3:
                 console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 4:
                 console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 5:
                 console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 6:
                 console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 7:
                 console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
-                break;
 
+                keyPress()
+                break;
             case 8:
                 console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
-                const conta = readlinesync.questionInt("Digite o número da conta: ");
-                const valor = readlinesync.questionInt("Digite o valor: ");
-                console.log(`Transferindo R$ ${valor} para a conta ${conta}`);
-                const confirmar = readlinesync.question("Deseja confirmar a operação (S/N): ");
-                if (confirmar.toLowerCase() === "s") {
-                    console.log(`Transferência realizada com sucesso. \nConta:${conta} \nValor:R$${valor}`);
-                } else {
-                    console.log("Operação cancelada.");
-                }
-                break;
 
+                keyPress()
+                break;
             default:
                 console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
+
+                keyPress()
                 break;
         }
-
-        keyPress();
     }
+
 }
 
-// Info do desenvolvedor
-export function sobre(): void {
+/* Função com os dados da pessoa desenvolvedora */
+function sobre(): void {
     console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: Raul Vieira");
-    console.log("Email: raul.vieiras2001@gmail.com");
-    console.log("GitHub: https://github.com/RaulVieira007/Projeto_Conta_Bancaria");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("github.com/conteudoGeneration");
     console.log("*****************************************************");
 }
 
-// Pausar até apertar ENTER
 function keyPress(): void {
-    console.log(colors.reset);
-    console.log("\nPressione ENTER para continuar...");
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
     readlinesync.prompt();
 }
 
-// Executa o programa
 main();
