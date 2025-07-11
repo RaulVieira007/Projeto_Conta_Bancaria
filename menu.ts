629cf13c1d2c2a2d694ca163c9a47fbf3aeeff42
RaulVieira007 <raul.vieiras2001@gmail.com>
2025-07-11 20:23:34 -0300

    update

--- menu.ts
+++ menu.ts
@@ -6,13 +6,6 @@ import { ContaPoupanca } from "./src/util/model/ContaPoupanca";
 export function main(): void {
     let opcao: number;
 
-    // const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    // conta.visualizar();
-    // conta.sacar(10500);
-    // conta.visualizar();
-    // conta.depositar(5000);
-    // conta.visualizar();
-
     const Contacorrente: ContaCorrente = new ContaCorrente (2, 123 , 1, "Mariana", 15000, 1000);
     Contacorrente.visualizar();
     Contacorrente.sacar(2000);
